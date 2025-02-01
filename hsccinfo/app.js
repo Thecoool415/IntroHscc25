f156be5ddbe1022c586e80f04dc1eb11d5324a97
Thecoool415 <colinmcknight1217@icloud.com>
2025-02-01 18:59:18 +0000

    test

--- hsccinfo/app.js
+++ hsccinfo/app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var bdpahistoryRouter = require('./routes/users')
 var app = express();
 
 // view engine setup
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/bdpahistory', bdpahistoryRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
